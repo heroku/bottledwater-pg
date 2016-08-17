8d7ecfbee46c4785c213b49c21730f92a92519fa
Sam Stokes <me@samstokes.co.uk>
2016-08-17 11:12:09 -0700

    Use Confluent Platform 3.0 APT repo for newer packages

--- debian/pbuilder-hooks/E15-apt.confluent.sh
+++ debian/pbuilder-hooks/E15-apt.confluent.sh
@@ -3,5 +3,5 @@
 # Add the Confluent APT repository for its bleeding-edge librdkafka package.
 
 apt-get install -y wget
-wget -qO - http://packages.confluent.io/deb/2.0/archive.key | apt-key add -
-echo 'deb [arch=amd64] http://packages.confluent.io/deb/2.0 stable main' > /etc/apt/sources.list.d/confluent.list
+wget -qO - http://packages.confluent.io/deb/3.0/archive.key | apt-key add -
+echo 'deb [arch=amd64] http://packages.confluent.io/deb/3.0 stable main' > /etc/apt/sources.list.d/confluent.list
